ed76601d4747f9ac555bb8107343094dfdcc292a
Imran Mammadli <murick@gmail.com>
2015-08-05 21:26:39 -0700

    Added scripts for graph database management

---
+++ scripts/graph-load.groovy
@@ -0,0 +1,173 @@
+
+inputFilePath   = "input/ntriples_200000_sorted.txt"
+
+ch_pound = "#"
+ch_slash = "/"
+ch_angle = ["<",">"]
+ch_quote = ['"',"'"]
+
+
+getRunTime = {beginTime, endTime -> 
+    //About:  returns duration between two moments
+    //Input:  timestamp1,  timestamp2
+    //Output: timestamp2 - timestamp1 in "hh:mm:ss.mss" format
+
+    def elapsedTime = endTime - beginTime
+    def hh = elapsedTime.intdiv(3600000);
+    def mm = (elapsedTime-hh*3600000).intdiv(60000);
+    def ss = (elapsedTime-hh*3600000-mm*60000).intdiv(1000);
+    def ms = (elapsedTime-hh*3600000-mm*60000-ss*1000);
+    
+    def str_hh = hh.toString()
+    def str_mm = mm.toString()
+    def str_ss = ss.toString()
+    def str_ms = ms.toString() 
+
+    if (hh < 10)  {str_hh = "0" + str_hh};
+    if (mm < 10)  {str_mm = "0" + str_mm};
+    if (ss < 10)  {str_ss = "0" + str_ss};
+    if (ms < 100) {str_ms = "0" + str_ms};
+    if (ms < 10)  {str_ms = "0" + str_ms};
+
+    def duration = str_hh+":"+str_mm+":"+str_ss+"."+str_ms
+    return duration
+}
+
+
+conf = {currTime -> new BaseConfiguration(){{
+    //About:  configures backend, index, output directory
+    //Input:  current system time in nanoseconds
+    //Output: N/A    
+    setProperty("storage.directory", "output/graph_storage"/*"output/[batch]"+currTime*/)
+    setProperty("storage.backend", "berkeleyje")    
+    setProperty("storage.batch-loading", false)
+
+    setProperty("index.search.backend", "elasticsearch")
+    setProperty("index.search.directory", "output/index_storage"/*"output/[index]"+currTime*/)
+    setProperty("index.search.elasticsearch.client-only", false)
+    setProperty("index.search.elasticsearch.local-mode", true)
+    }}
+}  
+
+
+parseLine = {
+    //About:  extracts subject, predicate, object, and predicate keyword
+    //Input:  set of triples separated by space or tabulation
+    //Output: array of values ["subject", "predicate", "object", "keyword"]
+    def elements = ["","","",""]
+    def separator = " "
+
+    try 
+    {
+        if (it.contains(">\t<")) {separator = "\t"}
+        elements[0] = it.substring(0,it.indexOf(separator))
+        elements[1] = it.substring(it.indexOf(separator)+1, it.indexOf(separator, it.indexOf(separator)+1))
+        elements[2] = it.substring(it.indexOf(separator, it.indexOf(separator)+1)+1, it.lastIndexOf(".")-1)
+
+        if (elements[1].contains(ch_slash) && 
+            elements[1].contains(ch_angle[0]) && 
+            elements[1].contains(ch_angle[1]))
+        {
+            elements[3] = elements[1].substring(elements[1].lastIndexOf(ch_slash)+1,elements[1].lastIndexOf(ch_angle[1]))
+            if (elements[3].contains(ch_pound)){elements[3] = elements[3].substring(elements[3].lastIndexOf(ch_pound)+1)}
+        }
+        else  {elements[3] = elements[1].toString()}
+        if ((elements[3] == "Label") || (elements[3] == "label")) {elements[3] = "titanLabel"}
+    }
+    catch(Throwable e) {elements[0] = "Parsing error: "; elements[1] = e.getMessage()}
+    finally {return elements}    
+}
+
+
+//Create and Configure Graph
+graph = TitanFactory.open(conf(System.nanoTime()))
+mgmt = graph.getManagementSystem()
+
+//Setup Property Keys
+uri = mgmt.makePropertyKey("uri").dataType(String.class).cardinality(Cardinality.SINGLE).make() //one value per URI key
+edge = mgmt.makeEdgeLabel("edge").multiplicity(MULTI).make() //multiple edges of the label between any pair of vertices
+
+//Establish Search Indices
+index1 = mgmt.buildIndex("EdgeCompositeIndex",Edge.class).addKey(uri).buildCompositeIndex()
+index2 = mgmt.buildIndex("VertexCompositeIndex",Vertex.class).addKey(uri).unique().buildCompositeIndex()
+index3 = mgmt.buildIndex("VertexMixedIndex",Vertex.class).addKey(uri/*,Mapping.STRING.getParameter()*/).buildMixedIndex("search")
+
+//Adjust Consistency Locks
+mgmt.setConsistency(uri,ConsistencyModifier.LOCK) //only one URI key per vertex
+mgmt.setConsistency(index2,ConsistencyModifier.LOCK) //URI uniqueness constraint
+mgmt.setConsistency(edge,ConsistencyModifier.FORK) //fork - conflict resolution mechanism for edges
+mgmt.commit()
+
+//Enable Batch Mode
+conf.setProperty("storage.batch-loading", true)
+graph.commit()
+batch = new BatchGraph(graph, VertexIDType.STRING, 100000)
+batch.setVertexIdKey("uri")
+
+//Prepair Report Variables
+txt_line = 1L
+err_line = 0
+err_size = 10
+errorList = new String[err_size]
+startTime = System.currentTimeMillis(); //HERE WE START
+
+//Read Input File
+numberOfTriples = 0L;
+println "\nCalculating number of triples in the file. Please wait...\n"; Thread.sleep(1000)
+new File(inputFilePath).eachLine({ 
+    if (!it.startsWith(ch_pound)) {numberOfTriples++;}
+})
+
+//Upload graph
+println "\n"+numberOfTriples+" triples found. Begin graph uploading...\n";
+new File(inputFilePath).eachLine({ 
+    if (!it.startsWith(ch_pound))
+    {
+        statement = parseLine(it)
+        if (statement[0]=="Parsing error: " && err_line < err_size)
+        {
+            errorList[err_line] = "Line "+txt_line+": "+statement[1]+"\n"
+            err_line++
+        }
+        else
+        {
+            subject   = statement[0]
+            predicate = statement[1]
+            object    = statement[2]
+            keyword   = statement[3]
+
+            def v1 = batch.getVertex(subject) ?: batch.addVertex(subject)
+            if (!object.startsWith(ch_quote[0]) && !object.startsWith(ch_quote[1]))
+            {
+                def v2 = batch.getVertex(object) ?: batch.addVertex(object)
+                def edge = batch.addEdge(null, v1, v2, "edge")
+                edge.setProperty("uri", predicate as String)
+            }
+            else
+            {
+                if(graph.getPropertyKey(keyword) == null)
+                {predicateKey = graph.makePropertyKey(keyword).dataType(String.class).cardinality(Cardinality.SINGLE).make()}
+                v1.setProperty(keyword, object.replaceAll(ch_quote[0],"").replaceAll(ch_quote[1],"") as String)
+                /*
+                conf.setProperty("storage.batch-loading", false)
+                graph.commit()
+                mgmt2 = graph.getManagementSystem()
+                if(mgmt2.getPropertyKey(keyword) == null)
+                {
+                    predicateKey = mgmt2.makePropertyKey(keyword).dataType(String.class).cardinality(Cardinality.SINGLE).make()
+                    predicateIdx = mgmt2.buildIndex(keyword,Vertex.class).addKey(predicateKey).buildMixedIndex("search")
+                }
+                mgmt2.commit(); conf.setProperty("storage.batch-loading", true); graph.commit()
+                v1.setProperty(keyword, object.replaceAll(ch_quote[0],"").replaceAll(ch_quote[1],"") as String)
+                */
+            }
+        }
+        if (++txt_line%100000L == 0L){println "${txt_line}/" + numberOfTriples +" triples : "+ getRunTime(startTime, System.currentTimeMillis())}       
+    }
+})
+
+batch.commit()
+println "\nTotal Runtime: " + getRunTime(startTime, System.currentTimeMillis())+"\n" //HERE WE STOP
+if (errorList[0]!=null) {println "Process Completed With Errors:\n"}
+for (i=0; i<err_size; i++) if (errorList[i]!=null) print errorList[i]
+  
---
+++ scripts/graph-query.groovy
@@ -0,0 +1,106 @@
+info = {
+    //About:  Help method. Displays method signatures
+    println "\n====== ONE CLUSTER ======="
+    println "Get summary for the given cluster:\tgetOne(x); x - URI of the given object"
+    println "List URI of the ads in the cluster:\tgetOne(x)[0].uri"
+    println "List URI of the phones in the cluster:\tgetOne(x)[1].uri"
+
+    println "\n====== ALL CLUSTERS ======"
+    println "Find all clusters:\t\t\tgetAll(block); block - optional parameter - shows how often to display progress"
+    println "Get number of detected clusters:\tset_clusters.size()"
+    println "Get summary for the i-th cluster:\tset_clusters.get(i)"
+    println "List URI of ads in the i-th cluster:\tset_clusters.get(i)[0].uri"
+    println "List URI of phones the in i-th cluster:\tset_clusters.get(i)[1].uri\n"
+}
+
+getRunTime = {beginTime, endTime -> 
+    //About:  returns duration between two moments
+    //Input:  timestamp1,  timestamp2
+    //Output: timestamp2 - timestamp1 in "hh:mm:ss.mss" format
+
+    def elapsedTime = endTime - beginTime
+    def hh = elapsedTime.intdiv(3600000);
+    def mm = (elapsedTime-hh*3600000).intdiv(60000);
+    def ss = (elapsedTime-hh*3600000-mm*60000).intdiv(1000);
+    def ms = (elapsedTime-hh*3600000-mm*60000-ss*1000);
+    
+    def str_hh = hh.toString()
+    def str_mm = mm.toString()
+    def str_ss = ss.toString()
+    def str_ms = ms.toString() 
+
+    if (hh < 10)  {str_hh = "0" + str_hh};
+    if (mm < 10)  {str_mm = "0" + str_mm};
+    if (ss < 10)  {str_ss = "0" + str_ss};
+    if (ms < 100) {str_ms = "0" + str_ms};
+    if (ms < 10)  {str_ms = "0" + str_ms};
+
+    def duration = str_hh+":"+str_mm+":"+str_ss+"."+str_ms
+    return duration
+}
+
+
+getOne = {uri -> 
+    //About:  detects a cluster - group of mutually connected objects (ads) and their shared properties (phones)
+    //Input:  URI of the initial object that belongs to the group
+    //Output: 2D-array of group properties: [X1, ..., Xm][Y1, ..., Yn] (set of advertisements and phone numbers)
+    //        m - number of objects (ads) in the cluster
+    //        n - number of shared properties (phone numbers)
+
+    def ads=[];    //set of advertisements in the group
+    def phones=[]; //set of phone numbers in the group
+
+    graph.V().has('uri',uri.toString()).as('start').aggregate(ads).outE.has('uri',edge1).inV.outE.has('uri',edge2).inV.outE().has('uri',edge3).as('e').inV().except(phones).filter {v, m -> v.inE().has('uri',edge3).except([m.e]).hasNext()}.store(phones).inE.has('uri',edge3).except('e').outV().inE.has('uri',edge2).outV.inE.has('uri',edge1).outV/*.except(ads)*/.loop('start') {true}.iterate();
+    return [ads, phones];
+}
+
+
+getAll = {blockSize ->
+    //About:  detects all clusters - groups of mutually connected objects (ads) and their shared properties (phones)
+    //Input:  display progress every "blockSize" lines
+    //Output: hashtable of cluster properties: [1 : [X1, ..., Xm][Y1, ..., Yn], ... k : [X1, ..., Xm][Y1, ..., Yn]]
+    //        m - number of objects (ads) in the cluster
+    //        n - number of shared properties (phone numbers)
+    //        k - total amount of clusters
+    
+    def pool    = [];         //set of ALL objects (advertisements)    
+    def idx_clusters = 0;     //number of obtained clusters so far
+    def idx_explored = 0;     //number of explored objects so far
+    def set_explored = [:];   //set of explored objects (advertisements) so far
+        set_clusters = [:];   //hashtable of clusterss
+    
+    println "\nCollecting objects. Please wait...\n"; Thread.sleep(1000)    
+    //graph.V.has("uri", com.thinkaurelius.titan.core.attribute.Text.REGEX, "[<](http)(.*)(processed>)").aggregate(pool).iterate();
+    graph.E.has("uri",edge1).outV.has("uri",CONTAINS, "<http>").aggregate(pool).iterate();
+
+    if (pool.size <= 0)
+    {
+        println "No objects found. Check the graph.\n";
+    }
+    else
+    {
+        startTime = System.currentTimeMillis();
+        if (blockSize == null || blockSize == 0) {blockSize = 100000};
+        println "\n"+pool.size+" objects collected. Begin analyzing...\n";
+
+        def lineNumber = 0L;
+        pool.each {
+            if (set_explored.containsValue(it) == false)
+            {   
+                def cluster = getOne(it.uri);
+                if (cluster[0].size() > 1) {set_clusters.put(++idx_clusters, cluster);}
+                cluster[0].each {set_explored.put(++idx_explored, it);}
+            }
+            if (++lineNumber%blockSize == 0L){println "${lineNumber}/" + pool.size + " objects : " + getRunTime(startTime, System.currentTimeMillis())}
+        }
+    }
+    println "\nAnalysis completed: "+idx_clusters+" cluster(s) found\n";
+    return set_clusters;
+}
+
+edge1 = '<http://memexproxy.com/ontology/hasFeatureCollection>';
+edge2 = '<http://memexproxy.com/ontology/phonenumber_feature>';
+edge3 = '<http://memexproxy.com/ontology/featureObject>';
+
+graph = TitanFactory.open('conf/titan-berkeleydb.properties');
+println "\nGraph loaded successfullly. Type info() for help."
